2a59ed75176781f80f9a7d94b7a739170c229d6b
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-08 15:04:34 +0200

    initialized html + js

---
+++ Ex palindroma/script.js
@@ -0,0 +1,5 @@
+/* Una funzione per capire se la parola è palindroma */
+
+
+// Definizione delle variabili
+console.log("Ciao a tutti!");
---
+++ Ex pari o dispari/script.js
@@ -0,0 +1,7 @@
+/* L’utente sceglie pari o dispari e un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer
+e dichiariamo chi ha vinto.*/
+
+
+// Definizione delle variabili
+console.log("Ciao Mondo!");
